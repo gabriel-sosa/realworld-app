95041289825b20276b03b5043c2e52d92a97e368
Gabriel Sosa <gabriel.sosa.developer@gmail.com>
2023-08-12 15:40:14 +0200

    handle zod errors

--- services/realworld-bff/src/middlewares/index.ts
+++ services/realworld-bff/src/middlewares/index.ts
@@ -1,4 +1,5 @@
 import { Pool } from "pg";
+import { ZodError } from "zod";
 import type { Request, Response, NextFunction } from "express";
 
 import { UserService } from "../services";
@@ -23,8 +24,23 @@ export const addContext = (config: Config) => {
   };
 };
 
-export const errorHandler = (err: unknown, _: Request, res: Response, next: NextFunction) => {
-  console.log(err);
-  res.send("bruh");
-  next();
+const zodErrorToStringArray = (err: ZodError): string[] => {
+  const { formErrors, fieldErrors } = err.flatten();
+
+  const formattedFieldErrors = Object.entries(fieldErrors).flatMap(
+    ([key, value]) => value?.map((val) => `validation error in ${key}: ${val}`) ?? [],
+  );
+
+  return [...formErrors, ...formattedFieldErrors];
+};
+
+// Even if we don't need the next function inside the error handler, express needs the function to have 4 arguments to know is an error handler
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export const errorHandler = async (err: unknown, _: Request, res: Response, __: NextFunction) => {
+  if (err instanceof ZodError) {
+    res.json({ error: zodErrorToStringArray(err) });
+    return;
+  }
+
+  res.json({ errors: ["Unexpected internal error"] });
 };
